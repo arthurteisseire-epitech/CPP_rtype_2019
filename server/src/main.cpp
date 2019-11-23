187c1cac91957fefc575f5c6cca6863cc6b1dc2d
arthur <arthur.teisseire@epitech.eu>
2019-11-23 18:46:49 +0100

    Add network directory in component and system directories

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,6 +5,7 @@
 ** main.cpp
 */
 
+#include <thread>
 #include "ConnectionSystem.hpp"
 #include "GetPool.hpp"
 #include "EntityAdmin.hpp"
@@ -14,6 +15,9 @@ int main()
     auto admin = std::make_shared<ecs::EntityAdmin>();
     ecs::ConnectionSystem connSystem(admin);
 
-    connSystem.update(0.16);
+    std::thread t([&]() {
+        connSystem.update(0.16);
+    });
+    t.join();
     std::cout << "end" << std::endl;
 }
