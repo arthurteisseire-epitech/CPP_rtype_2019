8d9d91fe282b4c85dee23e0d9b338437ead8d2ec
Arthamios <adrien.fabre@epitech.eu>
2019-11-24 18:37:57 +0100

    Put vec2f in direction component.

--- server/src/engine/component/DirectionComponent.hpp
+++ server/src/engine/component/DirectionComponent.hpp
@@ -8,17 +8,17 @@
 #ifndef RTYPE_DIRECTIONCOMPONENT_HPP
 #define RTYPE_DIRECTIONCOMPONENT_HPP
 
+#include "Vector2.hpp"
+
 namespace ecs
 {
     struct DirectionComponent {
         explicit DirectionComponent(int x = 0, int y = 0) :
-            x(x),
-            y(y)
+            dir(x, y)
         {
         }
 
-        int x;
-        int y;
+        mut::Vec2f dir;
     };
 }
 
