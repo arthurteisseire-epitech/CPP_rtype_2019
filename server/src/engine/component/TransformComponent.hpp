14882f0dccfd81933af355ba05524ff5e69ee6b6
Arthamios <adrien.fabre@epitech.eu>
2019-11-24 20:28:24 +0100

    Use vec2f in TransformComponent.

--- server/src/engine/component/TransformComponent.hpp
+++ server/src/engine/component/TransformComponent.hpp
@@ -8,17 +8,16 @@
 #ifndef RTYPE_TRANSFORMCOMPONENT_HPP
 #define RTYPE_TRANSFORMCOMPONENT_HPP
 
+#include "Vector2.hpp"
+
 namespace ecs
 {
     struct TransformComponent {
-        explicit TransformComponent(int x = 0, int y = 0) :
-            x(x),
-            y(y)
+        explicit TransformComponent(int x = 0, int y = 0) : vec(x, y)
         {
         }
 
-        int x;
-        int y;
+        mut::Vec2f vec;
     };
 }
 
