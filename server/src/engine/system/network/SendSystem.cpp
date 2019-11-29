50102306e3ad1d634e6690fa203ff195fc03fac7
arthur <arthur.teisseire@epitech.eu>
2019-11-29 11:33:22 +0100

    Use boost::bind in SendSystem

--- server/src/engine/system/network/SendSystem.cpp
+++ server/src/engine/system/network/SendSystem.cpp
@@ -5,6 +5,7 @@
 ** SendSystem.cpp
 */
 
+#include <boost/bind.hpp>
 #include "SendSystem.hpp"
 #include "NetworkUtil.hpp"
 #include "Util.hpp"
@@ -15,16 +16,20 @@ ecs::SendSystem::SendSystem(std::shared_ptr<EntityAdmin> admin) : ASystem(std::m
 
 void ecs::SendSystem::update(float deltaTime)
 {
-    std::array<char, 1024> buffer{};
-    ForEachMatching<SendRenderTuple>(admin,
-        [&buffer, this]
-            (SendRenderTuple &tuple,
-             ConnectionComponent &conn, TransformComponent &transform, TypeComponent &type, IdComponent &id) {
+    ForEachMatching<SendRenderTuple>(admin, boost::bind(&ecs::SendSystem::updateTuple, this, _1, _2, _3, _4, _5));
+}
 
-            auto s = std::to_string(id.id) + ";" + type.name + ":" + std::to_string(transform.vec.x) + "," +
-                std::to_string(transform.vec.y) + '\n';
-            buffer.fill(0);
-            std::copy(s.begin(), s.end(), buffer.begin());
-            NetworkUtil::send(admin, GetIndex<ConnectionComponent>(tuple), buffer);
-        });
+void ecs::SendSystem::updateTuple(ecs::SendRenderTuple &tuple,
+                                  ecs::ConnectionComponent &conn,
+                                  ecs::TransformComponent &transform,
+                                  ecs::TypeComponent &type,
+                                  ecs::IdComponent &id)
+{
+    std::array<char, 1024> buffer{};
+
+    auto s = std::to_string(id.id) + ";" + type.name + ":" + std::to_string(transform.vec.x) + "," +
+        std::to_string(transform.vec.y) + '\n';
+    buffer.fill(0);
+    std::copy(s.begin(), s.end(), buffer.begin());
+    NetworkUtil::send(admin, GetIndex<ConnectionComponent>(tuple), buffer);
 }
--- server/src/engine/system/network/SendSystem.hpp
+++ server/src/engine/system/network/SendSystem.hpp
@@ -17,6 +17,11 @@ namespace ecs
         explicit SendSystem(std::shared_ptr<EntityAdmin> admin);
 
         void update(float deltaTime) override;
+        void updateTuple(SendRenderTuple &tuple,
+                         ConnectionComponent &conn,
+                         TransformComponent &transform,
+                         TypeComponent &type,
+                         IdComponent &id);
     };
 }
 
