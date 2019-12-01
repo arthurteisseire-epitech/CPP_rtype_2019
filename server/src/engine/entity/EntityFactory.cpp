4e09485960b02bf3de0d8f91fd1b68365eed6011
arthur <arthur.teisseire@epitech.eu>
2019-12-01 09:26:04 +0100

    [server] Change the way to init Entity from a set of tuples to a set of components (tuples are auto-generated in Entity)

--- server/src/engine/entity/EntityFactory.cpp
+++ server/src/engine/entity/EntityFactory.cpp
@@ -18,16 +18,13 @@ int ecs::EntityFactory::id = 0;
 void ecs::EntityFactory::createPlayer(std::shared_ptr<EntityAdmin> &admin,
                                       ObjectPool<CConnection>::index connIdx)
 {
-    auto transformIdx = GetPool<CTransform>(admin).create();
-    auto dirIdx = GetPool<CDirection>(admin).create();
-    auto typeIdx = GetPool<CType>(admin).create("spaceship_normal");
-    auto objIdIdx = GetPool<CId>(admin).create(id++);
-
     admin->entities.emplace_back(
         new Entity(admin,
-            GetPool<InputTuple>(admin).create(connIdx, transformIdx, dirIdx),
-            GetPool<MoveTuple>(admin).create(transformIdx, dirIdx),
-            GetPool<SendRenderTuple>(admin).create(connIdx, transformIdx, typeIdx, objIdIdx)
+            connIdx,
+            GetPool<CTransform>(admin).create(),
+            GetPool<CDirection>(admin).create(),
+            GetPool<CType>(admin).create("spaceship_normal"),
+            GetPool<CId>(admin).create(id++)
         )
     );
 }
@@ -37,14 +34,15 @@ void ecs::EntityFactory::createBullet(std::shared_ptr<EntityAdmin> &admin,
                                       ObjectPool<CTransform>::index transformIdx)
 {
     auto directionIdx = GetPool<CDirection>(admin).create();
-    auto typeIdx = GetPool<CType>(admin).create("basic_missile_launch");
-    auto objIdIdx = GetPool<CId>(admin).create(id++);
 
     GetPool<CDirection>(admin).at(directionIdx).setDirection(CDirection::RIGHT);
     admin->entities.emplace_back(
         new Entity(admin,
-            GetPool<MoveTuple>(admin).create(transformIdx, directionIdx),
-            GetPool<SendRenderTuple>(admin).create(connectionIdx, transformIdx, typeIdx, objIdIdx)
+            GetPool<CType>(admin).create("basic_missile_launch"),
+            GetPool<CId>(admin).create(id++),
+            connectionIdx,
+            transformIdx,
+            directionIdx
         )
     );
 }
---
+++ server/src/engine/util/TupleTypeUtil.hpp
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2019
+** rtype
+** File description:
+** MetaUtil.hpp
+*/
+
+#ifndef RTYPE_TUPLETYPEUTIL_HPP
+#define RTYPE_TUPLETYPEUTIL_HPP
+
+#include <tuple>
+#include "MoveTuple.hpp"
+#include "InputTuple.hpp"
+
+namespace ecs
+{
+    template<typename T, typename Tuple>
+    struct has_type;
+
+    template<typename T, typename... Us>
+    struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...> {};
+
+    template<typename SubTuple, typename Tuple>
+    struct has_types;
+
+    template<typename ...T, typename Tuple>
+    struct has_types<std::tuple<T...>, Tuple> : std::conjunction<has_type<T, Tuple>...> {};
+
+    // TODO -- Template recursion seems to be a better way to achieve this
+    template<typename IdxTuple, typename ...Ts>
+    auto getTuplesMatching(const std::tuple<Ts...> &t)
+    {
+        return std::apply([](Ts...) {
+            return std::tuple_cat(std::conditional_t<(has_types<Ts, IdxTuple>::value),
+                                                     std::tuple<typename ObjectPool<Ts>::index>,
+                                                     std::tuple<>
+                                                    >{}...);
+        }, t);
+    }
+
+    // TODO -- Remove Tuples duplication
+    template<typename ...Args>
+    using EntityTuples = decltype(getTuplesMatching
+        <
+            decltype(std::tuple<Args...>{}),
+            MoveTuple,
+            InputTuple,
+            SendRenderTuple
+        >
+        (std::tuple
+            <
+                MoveTuple,
+                InputTuple,
+                SendRenderTuple
+            >{}));
+}
+
+#endif
